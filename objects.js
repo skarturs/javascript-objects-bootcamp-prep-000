39ee776637cbc071f3479da19361aac3a89721b4
Arturs Teihmanis <arturs.teihmanis@gmail.com>
2017-11-27 15:33:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,13 +2,10 @@ let playlist = {
   artistName: songTitle
 };
 
-function updatePlaylist(playlist,artistName, songTitle){
-return playlist.assign({'Phil Ochs':"Here's to the State of Mississippi"});
+function updatePlaylist(playlist, artistName, songTitle){
+return playlist.assign({},playlist,{['Phil Ochs']:"Here's to the State of Mississippi"});
 };
 
-function updateObjectWithObject(targetObject, updatesObject) {
-  return Object.assign({}, targetObject, updatesObject)
-}
 
 function removeFromPlaylist(playlist,artistName){
 delete playlist.artistName;
