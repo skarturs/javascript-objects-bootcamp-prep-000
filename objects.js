db608769de0ad852c449797723339941e95e4f05
Arturs Teihmanis <arturs.teihmanis@gmail.com>
2017-11-27 15:50:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,9 +3,7 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle){
-playlist.artistName = 'Phil Ochs';
-playlist.songTitle = "Here's to the State of Mississippi";
-return this.playlist;
+return playlist.assign(playlist, artistName, songTitle);
 };
 
 
