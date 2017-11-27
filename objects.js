145b44c269c277b3044238157ce18cf63190dc19
Arturs Teihmanis <arturs.teihmanis@gmail.com>
2017-11-27 16:13:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,8 @@
 var playlist = { artistName: 'songTitle'};
 
 function updatePlaylist(playlist, artistName, songTitle){
-return Object.assign(playlist, artistName, songTitle);
+playlist[artistName] = songTitle
+return playlist
 };
 
 function removeFromPlaylist(playlist,artistName){
